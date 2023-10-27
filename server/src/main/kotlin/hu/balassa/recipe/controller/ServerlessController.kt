94040c8084ec54d077d60ebec19802784734485c
Adam Balassa <balassaadi@gmail.com>
2023-10-27 18:00:29 +0300

    Whateethis is

---
+++ server/src/main/kotlin/hu/balassa/recipe/controller/ServerlessController.kt
@@ -0,0 +1,3 @@
+package hu.balassa.recipe.controller
+
+fun listRecipes =
---
+++ server/src/main/kotlin/hu/balassa/recipe/util/LambdaHelpers.kt
@@ -0,0 +1,5 @@
+package hu.balassa.recipe.util
+
+fun <T, B>lambda(endpoint: (pathParams: Map<String, String>, body: B) -> T) {
+
+}
